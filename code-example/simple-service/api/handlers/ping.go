2cfc19b4992dadb63dda114d4adde61c04d6f729
André Schaffer <andre.schaffer@gmail.com>
2023-03-03 11:21:11 +0100

    Allow api to return error

--- code-example/simple-service/api/handlers/ping.go
+++ code-example/simple-service/api/handlers/ping.go
@@ -3,10 +3,29 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 )
 
 func Ping() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "pong")
+		queryParams := req.URL.Query()
+
+		var err error
+		fail := false
+
+		failParam := queryParams.Get("fail")
+		if failParam != "" {
+			fail, err = strconv.ParseBool(failParam)
+			if err != nil {
+				http.Error(w, "invalid query param 'fail' value", http.StatusBadRequest)
+				return
+			}
+		}
+
+		if fail {
+			http.Error(w, "failed as requested!", http.StatusInternalServerError)
+		} else {
+			fmt.Fprintf(w, "pong")
+		}
 	})
 }
