92cc6cbc670d454cdc6f88fcefffeefb6ad14abb
André Schaffer <andre.schaffer@gmail.com>
2023-03-04 19:34:47 +0100

    Add sleep param

--- code-example/simple-service/internal/api/handlers/ping.go
+++ code-example/simple-service/internal/api/handlers/ping.go
@@ -4,28 +4,49 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 func Ping() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		queryParams := req.URL.Query()
-
-		var err error
-		fail := false
-
-		failParam := queryParams.Get("fail")
-		if failParam != "" {
-			fail, err = strconv.ParseBool(failParam)
-			if err != nil {
-				http.Error(w, "invalid query param 'fail' value", http.StatusBadRequest)
-				return
-			}
+		fail, err := parseFailParam(req)
+		if err != nil {
+			http.Error(w, "invalid query param 'fail' value", http.StatusBadRequest)
+			return
 		}
+
+		sleep, err := parseSleepParam(req)
+		if err != nil {
+			http.Error(w, "invalid query param 'sleepMs' value", http.StatusBadRequest)
+			return
+		}
+
+		time.Sleep(sleep)
 
 		if fail {
-			http.Error(w, "failed as requested!", http.StatusInternalServerError)
+			http.Error(w, "no pong, failed as requested!", http.StatusInternalServerError)
 		} else {
-			fmt.Fprintf(w, "pong")
+			fmt.Fprintf(w, "pong!")
 		}
 	})
+}
+
+func parseFailParam(req *http.Request) (bool, error) {
+	failParam := req.URL.Query().Get("fail")
+
+	if failParam == "" {
+		return false, nil
+	}
+
+	return strconv.ParseBool(failParam)
+}
+
+func parseSleepParam(req *http.Request) (time.Duration, error) {
+	sleepParam := req.URL.Query().Get("sleep")
+
+	if sleepParam == "" {
+		return 0, nil
+	}
+
+	return time.ParseDuration(sleepParam)
 }
