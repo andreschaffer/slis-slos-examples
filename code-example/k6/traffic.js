8685d02f0b473dc81f9eeada16c3add0a2f38b07
André Schaffer <andre.schaffer@gmail.com>
2023-10-30 13:59:57 +0100

    Add k6 for simulating traffic

---
+++ code-example/k6/traffic.js
@@ -0,0 +1,10 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export default function () {
+  const failure = Math.random() >= 0.99 ? true : false;
+  const sleepMs = Math.random() >= 0.99 ? 100 : 0;
+
+  http.get(`http://host.docker.internal:8888/ping?fail=${failure}&sleep=${sleepMs}ms`);
+  sleep(1);
+}
